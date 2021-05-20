5cc6b0d6a38882650e926243a3296352ddeb35af
vwedesam <eshemiedomisam@yahoo.com>
2021-05-20 22:10:11 +0100

    feat: add CORS support

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ app = express();
 const port = process.env.PORT || 3210;
 
 app.use(express.json());
-app.get('/store-sample', (req, res)=> {
+app.get('/store-sample', (req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (req.method == "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Credentials", "'true");
+        res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, x-access-token, x-api-token, X-Requested-With, Content-Type, Accept");
+        res.setHeader("Access-Control-Max-Age", "3800");
+
+        res.status(200).end();
+    }
 
     let limit = req.query.limit || 10
     const data = extract(limit);
@@ -12,6 +23,6 @@ app.get('/store-sample', (req, res)=> {
     res.json(data);
 });
 
-app.listen(port, ()=>{
-    console.log("server running on port "+ port);
+app.listen(port, () => {
+    console.log("server running on port " + port);
 });
